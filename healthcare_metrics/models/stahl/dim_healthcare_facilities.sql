f9f394ddc113199907ed5297e3daf14a7d29f24a
thecartercodes <thecarter@tm41m.com>
2023-10-23 16:35:54 -0600

    simpliy point transformation

--- healthcare_metrics/models/stahl/dim_healthcare_facilities.sql
+++ healthcare_metrics/models/stahl/dim_healthcare_facilities.sql
@@ -7,13 +7,11 @@ select
     , coalesce(cd.cdname, 'Unknown') as census_division_name
 from {{ source('statcan', 'healthcare_facilities') }} as hf
 left join {{ source('statcan', 'census_divisions') }} as cd on st_covers(
-    cd.geom, st_transform(
-        st_setsrid(
-            st_makepoint(
-                hf.longitude::double precision
-                , hf.latitude::double precision
-            )
-            , 4326
+    cd.geom
+    , st_setsrid(
+        st_makepoint(
+            hf.longitude::double precision
+            , hf.latitude::double precision
         )
         , 3347
     )
